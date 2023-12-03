2592ad032bddec747f7d127f2fc214bb83220a16
SimplyFlawed <72576623+SimplyFlawed@users.noreply.github.com>
2023-12-02 20:29:03 -0500

    Added header comment

--- scenes/LevelC.cpp
+++ scenes/LevelC.cpp
@@ -1,3 +1,13 @@
+/**
+* Author: Raymond Lin
+* Assignment: Platformer
+* Date due: 2023-12-02, 11:59pm
+* I pledge that I have completed this assignment without
+* collaborating with anyone else, in conformance with the
+* NYU School of Engineering Policies and Procedures on
+* Academic Misconduct.
+**/
+
 #include "LevelC.h"
 #include "../Utility.h"
 
